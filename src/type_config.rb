681dd3142821ca6e8580bc56d2246dec2972be8f
Miles Georgi <azimux@gmail.com>
2024-03-19 16:49:40 -0700

    Allow passing organization name on the command line

--- src/type_config.rb
+++ src/type_config.rb
@@ -15,10 +15,11 @@ module Foobara
           type :string, default: TypeType::TYPE, one_of: TypeType
           description :string, :allow_nil
           domain :string, :allow_nil
+          organization :string, :allow_nil
         end
 
         def module_path
-          @module_path ||= [*domain&.split("::"), *name.split("::")]
+          @module_path ||= [*organization&.split("::"), *domain&.split("::"), *name.split("::")]
         end
       end
     end
